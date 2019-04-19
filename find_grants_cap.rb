3bae0b62844adfc51bfbec42b642dda194dc37ce
Quynh Yen Vo T <quynhyen.vothi@gmail.com>
2019-04-20 00:53:39 +0200

    Create find_grants_cap.rb

---
+++ find_grants_cap.rb
@@ -0,0 +1,52 @@
+=begin
+
+PRAMP
+Award Budget Cuts
+The awards committee of your alma mater (i.e. your college/university) asked for your assistance with a budget allocation problem they’re facing. Originally, the committee planned to give N research grants this year. However, due to spending cutbacks, the budget was reduced to newBudget dollars and now they need to reallocate the grants. The committee made a decision that they’d like to impact as few grant recipients as possible by applying a maximum cap on all grants. Every grant initially planned to be higher than cap will now be exactly cap dollars. Grants less or equal to cap, obviously, won’t be impacted.
+Given an array grantsArray of the original grants and the reduced budget newBudget, write a function findGrantsCap that finds in the most efficient manner a cap such that the least number of recipients is impacted and that the new budget constraint is met (i.e. sum of the N reallocated grants equals to newBudget).
+Analyze the time and space complexities of your solution.
+Example:
+input:  grantsArray = [2, 100, 50, 120, 1000], newBudget = 190
+output: 47 # and given this cap the new grants array would be
+           # [2, 47, 47, 47, 47]. Notice that the sum of the
+           # new grants is indeed 190
+Constraints:
+[time limit] 5000ms
+[input] array.double grantsArray
+0 ≤ grantsArray.length ≤ 20
+0 ≤ grantsArray[i]
+[input] double newBudget
+[output] double
+
+=end
+def find_grants_cap(grantsArray, newBudget)
+  # your code goes here 
+  grantsArray.sort!{|a,b| a<=>b}
+  n = grantsArray.length
+  remaining_budget = newBudget.round(2).to_f
+  cap =  remaining_budget/n
+  for i in (0..n-1)
+    if (i == grantsArray.length-1 ) || (cap == 0.0)
+      break
+    end
+    if  grantsArray[i] >= cap
+      n -=1
+      remaining_budget -= cap
+      cap = remaining_budget/n
+      
+    else
+       n -=1
+      remaining_budget -= grantsArray[i]
+      cap =  remaining_budget/n
+     
+    end
+    
+   
+  end
+  return cap.round(2)
+end
+
+ grantsArray = [2, 100, 50, 120, 1000]
+
+ newBudget = 190
+p find_grants_cap(grantsArray, newBudget)
